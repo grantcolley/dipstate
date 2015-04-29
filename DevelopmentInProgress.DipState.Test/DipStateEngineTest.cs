cca155152a3ff131e866d7d2f6d5f9de2533440e
grantcolley <grantcolley@yahoo.com>
2015-04-29 23:27:50 +0100

    Unit tests in progress
    Unit tests in progress

--- DevelopmentInProgress.DipState.Test/DipStateEngineTest.cs
+++ DevelopmentInProgress.DipState.Test/DipStateEngineTest.cs
@@ -21,7 +21,8 @@ namespace DevelopmentInProgress.DipState.Test
         public void Run_StateAlreadyHasStatusSpecified_StateStatusUnchanged()
         {
             // Arrange
-            IDipState state = new DipState(1, "Pricing Workflow", DipStateType.Standard, DipStateStatus.Initialised);
+            IDipState state = new DipState(1, "Pricing Workflow", DipStateType.Standard,
+                status: DipStateStatus.Initialised);
 
             // Act
             state = dipStateEngine.Run(state, DipStateStatus.Initialised);
@@ -53,11 +54,11 @@ namespace DevelopmentInProgress.DipState.Test
             // Arrange
             var mockAction = new Mock<Action<IDipState>>();
 
-            var state = new DipState(1, "Pricing Workflow")
+            IDipState state = new DipState(1, "Pricing Workflow")
                 .AddAction(DipStateActionType.Entry, mockAction.Object);
 
             // Act
-            state = dipStateEngine.Run(state, DipStateStatus.Initialised) as DipState;
+            state = dipStateEngine.Run(state, DipStateStatus.Initialised);
 
             // Assert
             mockAction.Verify(a => a(state), Times.Once);
@@ -73,7 +74,7 @@ namespace DevelopmentInProgress.DipState.Test
             // Arrange 
             var mockAction = new Mock<Action<IDipState>>();
 
-            var state = new DipState(1, "Pricing Workflow")
+            IDipState state = new DipState(1, "Pricing Workflow")
                 .AddAction(DipStateActionType.Entry, mockAction.Object);
 
             mockAction.Setup(a => a(state)).Throws(new InvalidOperationException("Run_InitialiseStateWithExceptionInEntryAction_ExceptionThrownStateNotInitialised"));
@@ -81,7 +82,7 @@ namespace DevelopmentInProgress.DipState.Test
             // Act
             try
             {
-                state = dipStateEngine.Run(state, DipStateStatus.Initialised) as DipState;
+                state = dipStateEngine.Run(state, DipStateStatus.Initialised);
             }
             catch (InvalidOperationException ex)
             {
@@ -102,9 +103,9 @@ namespace DevelopmentInProgress.DipState.Test
         public void Run_InitialiseStateWithDependencyComplete_StateInitialised()
         {
             // Arrange
-            var dependency = new DipState(0, "Dependent State", DipStateType.Standard, DipStateStatus.Completed);
+            var dependency = new DipState(0, "Dependent State", DipStateType.Standard, status: DipStateStatus.Completed);
 
-            var state = new DipState(1, "Pricing Workflow")
+            IDipState state = new DipState(1, "Pricing Workflow")
                 .AddDependency(dependency);
 
             // Act
@@ -121,9 +122,9 @@ namespace DevelopmentInProgress.DipState.Test
         public void Run_InitialiseStateWithDependencyNotComplete_StateNotInitialised()
         {
             // Arrange
-            var dependency = new DipState(0, "Dependent State", DipStateType.Standard, DipStateStatus.InProgress);
+            var dependency = new DipState(0, "Dependent State", DipStateType.Standard, status: DipStateStatus.InProgress);
 
-            var state = new DipState(1, "Pricing Workflow")
+            IDipState state = new DipState(1, "Pricing Workflow")
                 .AddDependency(dependency);
 
             // Act
@@ -141,19 +142,108 @@ namespace DevelopmentInProgress.DipState.Test
         [TestMethod]
         public void Run_InitialiseAutoStateAndComplete_AutoStateCompleted()
         {
-            throw new NotImplementedException();
+            // Arrange
+            IDipState autoState = new DipState(1, "Override Check", DipStateType.Auto);
+
+            // Act
+            autoState = dipStateEngine.Run(autoState, DipStateStatus.Initialised);
+
+            // Assert
+            Assert.AreEqual(autoState.Id, 1);
+            Assert.AreEqual(autoState.Name, "Override Check");
+            Assert.AreEqual(autoState.Status, DipStateStatus.Completed);
         }
 
         [TestMethod]
-        public void Run_InitialiseAutoStateAndAutoTransition_AutoStateTransitioned()
+        public void Run_InitialiseAutoStateAndAutoTransition_AutoStateTransitionedToFinal()
         {
-            throw new NotImplementedException();
+            // Arrange
+            var finalState = new DipState(2, "Final");
+            var overrideState = new DipState(3, "Override");
+
+            IDipState autoState = new DipState(1, "Override Check", DipStateType.Auto)
+                .AddTransition(finalState)
+                .AddTransition(overrideState)
+                .AddAction(DipStateActionType.Entry, s => { s.Transition = finalState; });
+
+            // Act
+            var state = dipStateEngine.Run(autoState, DipStateStatus.Initialised);
+
+            // Assert
+            Assert.AreEqual(autoState.Id, 1);
+            Assert.AreEqual(autoState.Name, "Override Check");
+            Assert.AreEqual(autoState.Status, DipStateStatus.Completed);
+
+            Assert.AreEqual(state.Id, 2);
+            Assert.AreEqual(state.Name, "Final");
+            Assert.AreEqual(state.Status, DipStateStatus.Initialised);
+
+            Assert.AreEqual(overrideState.Id, 3);
+            Assert.AreEqual(overrideState.Name, "Override");
+            Assert.AreEqual(overrideState.Status, DipStateStatus.Uninitialised);
+        }
+
+        [TestMethod]
+        public void Run_InitialiseAutoStateAndAutoTransition_AutoStateTransitionedToOverride()
+        {
+            // Arrange
+            var finalState = new DipState(2, "Final");
+            var overrideState = new DipState(3, "Override");
+
+            IDipState autoState = new DipState(1, "Override Check", DipStateType.Auto)
+                .AddTransition(finalState)
+                .AddTransition(overrideState)
+                .AddAction(DipStateActionType.Entry, s => { s.Transition = overrideState; });
+
+            // Act
+            var state = dipStateEngine.Run(autoState, DipStateStatus.Initialised);
+
+            // Assert
+            Assert.AreEqual(autoState.Id, 1);
+            Assert.AreEqual(autoState.Name, "Override Check");
+            Assert.AreEqual(autoState.Status, DipStateStatus.Completed);
+
+            Assert.AreEqual(finalState.Id, 2);
+            Assert.AreEqual(finalState.Name, "Final");
+            Assert.AreEqual(finalState.Status, DipStateStatus.Uninitialised);
+
+            Assert.AreEqual(state.Id, 3);
+            Assert.AreEqual(state.Name, "Override");
+            Assert.AreEqual(state.Status, DipStateStatus.Initialised);
         }
 
         [TestMethod]
         public void Run_InitialiseAggregateStateInitialiseTwoSubStatesWithParent_AggregateStateInitialisedTwoSubStatesInitialised()
         {
-            throw new NotImplementedException();
+            // Arrange
+            IDipState state = new DipState(1, "Pricing Workflow")
+                .AddSubState(new DipState(2, "Pricing A", initialiseWithParent: true))
+                .AddSubState(new DipState(3, "Pricing B"))
+                .AddSubState(new DipState(4, "Pricing C", initialiseWithParent: true));
+
+            // Act
+            state = dipStateEngine.Run(state, DipStateStatus.Initialised);
+
+            // Assert
+            Assert.IsNotNull(state);
+            Assert.AreEqual(state.Id, 1);
+            Assert.AreEqual(state.Name, "Pricing Workflow");
+            Assert.AreEqual(state.Status, DipStateStatus.Initialised);
+
+            var pricingA = state.SubStates.First(pa => pa.Name.Equals("Pricing A"));
+            Assert.AreEqual(pricingA.Id, 2);
+            Assert.AreEqual(pricingA.Name, "Pricing A");
+            Assert.AreEqual(pricingA.Status, DipStateStatus.Initialised);
+
+            var pricingB = state.SubStates.First(pa => pa.Name.Equals("Pricing B"));
+            Assert.AreEqual(pricingB.Id, 3);
+            Assert.AreEqual(pricingB.Name, "Pricing B");
+            Assert.AreEqual(pricingB.Status, DipStateStatus.Uninitialised);
+
+            var pricingC = state.SubStates.First(pa => pa.Name.Equals("Pricing C"));
+            Assert.AreEqual(pricingC.Id, 4);
+            Assert.AreEqual(pricingC.Name, "Pricing C");
+            Assert.AreEqual(pricingC.Status, DipStateStatus.Initialised);
         }
     }
 }
