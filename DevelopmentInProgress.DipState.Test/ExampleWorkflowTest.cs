83fe6a45e374788f04fd28d326a5d011fded5630
grantcolley <grantcolley@yahoo.com>
2015-05-04 23:29:25 +0100

    Add stub for testing comprehensive workflow example
    Add stub for testing comprehensive real world workflow example.

---
+++ DevelopmentInProgress.DipState.Test/ExampleWorkflowTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevelopmentInProgress.DipState.Test
+{
+    [TestClass]
+    public class ExampleWorkflowTest
+    {
+        private DipStateEngine dipStateEngine;
+        private IDipState pricingWorkflow;
+        private IDipState collateData;
+        private IDipState modelling;
+        private IDipState modelling1;
+        private IDipState modelling2;
+        private IDipState reviewModelling;
+        private IDipState adjustmentCheck;
+        private IDipState adjustments;
+        private IDipState finalReview;
+
+        [TestInitialize]
+        public void Initialise()
+        {
+            dipStateEngine = new DipStateEngine();
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
