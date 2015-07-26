defc97c30e325755247845b245c17719f5df33d6
grantcolley <grantcolley@yahoo.com>
2015-07-26 23:13:26 +0100

    Delete the DipStateEngine class
    Delete the DipStateEngine class

--- DevelopmentInProgress.DipState/IDipStateEngine.cs
+++ DevelopmentInProgress.DipState/IDipStateEngine.cs
@@ -1,15 +1,15 @@
-﻿using System.Threading.Tasks;
+﻿//using System.Threading.Tasks;
 
-namespace DevelopmentInProgress.DipState
-{
-    public interface IDipStateEngine
-    {
-        DipState Run(DipState state, DipStateStatus newStatus);
-        DipState Run(DipState state, DipState transitionState);
-        DipState Run(DipState state, DipStateStatus newStatus, DipState transitionState);
+//namespace DevelopmentInProgress.DipState
+//{
+//    public interface IDipStateEngine
+//    {
+//        DipState Run(DipState state, DipStateStatus newStatus);
+//        DipState Run(DipState state, DipState transitionState);
+//        DipState Run(DipState state, DipStateStatus newStatus, DipState transitionState);
 
-        Task<DipState> RunAsync(DipState state, DipStateStatus newStatus);
-        Task<DipState> RunAsync(DipState state, DipState transitionState);
-        Task<DipState> RunAsync(DipState state, DipStateStatus newStatus, DipState transitionState);
-    }
-}
+//        Task<DipState> RunAsync(DipState state, DipStateStatus newStatus);
+//        Task<DipState> RunAsync(DipState state, DipState transitionState);
+//        Task<DipState> RunAsync(DipState state, DipStateStatus newStatus, DipState transitionState);
+//    }
+//}
