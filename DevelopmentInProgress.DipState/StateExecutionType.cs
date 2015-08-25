1de674a192793646707e269927b08801ecfc0ca1
grantcolley <grantcolley@yahoo.com>
2015-08-25 22:45:09 +0100

    Add StateExecutionType
    Add StateExecutionType

---
+++ DevelopmentInProgress.DipState/StateExecutionType.cs
@@ -0,0 +1,28 @@
+﻿namespace DevelopmentInProgress.DipState
+{
+    /// <summary>
+    /// The execution action of the state.
+    /// </summary>
+    public enum StateExecutionType
+    {
+        /// <summary>
+        /// Resets the state.
+        /// </summary>
+        Reset = 1,
+
+        /// <summary>
+        /// Initialise the state.
+        /// </summary>
+        Initialise = 2,
+
+        /// <summary>
+        /// Sets the state to in progress.
+        /// </summary>
+        InProgress = 3,
+
+        /// <summary>
+        /// Completes the state.
+        /// </summary>
+        Complete = 4
+    }
+}
